320f4a6318d9dd90730c6244d7eb9ba57c3314d0
Lamarks Cavalcanti <lamarks.cavalcanti@gmail.com>
2024-05-09 22:44:31 -0300

    Final Commit

--- ExemploFastAPIDocker/atleta/models.py
+++ ExemploFastAPIDocker/atleta/models.py
@@ -1,16 +1,21 @@
 from datetime import datetime
-from sqlalchemy import DateTime, Integer, String, Float
-from sqlalchemy.orm import Mapped, mapped_column
-import ExemploFastAPIDocker.contrib.models import BaseModel
+from sqlalchemy import DateTime, ForeignKey, Integer, String, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from not_used.contrib.models import BaseModel
 
-class AtletaModel():
-    __tablename__ = 'Atleta'
 
-    pk_id: Mapped[int] = mapped_columm(Integer, primary_key=True)
-    nome: Mapped[str] = mapped_columm(String(50), nullable=False)
-    cpf: Mapped[str] = mapped_columm(String(11), nullable=False)
-    idade: Mapped[int] = mapped_columm(Integer, nullable=False)
-    peso: Mapped[float] = mapped_columm(Float, nullable=False)
-    altura: Mapped[float] = mapped_columm(Float, nullable=False)
-    sexo: Mapped[str] = mapped_columm(String(1), nullable=False)
-    created_at: Mapped[datetime] = mapped_columm(Datetime, nullable=False)
+class AtletaModel(BaseModel):
+    __tablename__ = 'atletas'
+
+    pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    nome: Mapped[str] = mapped_column(String(50), nullable=False)
+    cpf: Mapped[str] = mapped_column(String(11), unique=True, nullable=False)
+    idade: Mapped[int] = mapped_column(Integer, nullable=False)
+    peso: Mapped[float] = mapped_column(Float, nullable=False)
+    altura: Mapped[float] = mapped_column(Float, nullable=False)
+    sexo: Mapped[str] = mapped_column(String(1), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    categoria: Mapped['CategoriaModel'] = relationship(back_populates="atleta", lazy='selectin')
+    categoria_id: Mapped[int] = mapped_column(ForeignKey("categorias.pk_id"))
+    centro_treinamento: Mapped['CentroTreinamentoModel'] = relationship(back_populates="atleta", lazy='selectin')
+    centro_treinamento_id: Mapped[int] = mapped_column(ForeignKey("centros_treinamento.pk_id"))
--- ExemploFastAPIDocker/atleta/schemas.py
+++ ExemploFastAPIDocker/atleta/schemas.py
@@ -1,10 +1,29 @@
-from pydantic import BaseModel, Field, PositiveFloat
-from typing import Annotated
+from typing import Annotated, Optional
+from pydantic import Field, PositiveFloat
+from not_used.categorias.schemas import CategoriaIn
+from not_used.centro_treinamento.schemas import CentroTreinamentoAtleta
 
-class Atleta(BaseModel):
-    nome: Annotated[str, Field(description='Nome do atleta', examples='Joao', max_length=50)]
-    cpf: Annotated[str, Field(description='CPF do atleta', examples='12345678900', max_length=11)]
-    idade: Annotated[int, Field(description='Idade do atleta', examples=25)]
-    peso: Annotated[PositiveFloat, Field(description='Peso do atleta', examples=75.5)]
-    altura: Annotated[PositiveFloat, Field(description='Altura do atleta', examples=1.79)]
-    sexo: Annotated[str, Field(description='Sexo do atleta', examples='M', max_length=1)]
+from not_used.contrib.schemas import BaseSchema, OutMixin
+
+
+class Atleta(BaseSchema):
+    nome: Annotated[str, Field(description='Nome do atleta', example='Joao', max_length=50)]
+    cpf: Annotated[str, Field(description='CPF do atleta', example='12345678900', max_length=11)]
+    idade: Annotated[int, Field(description='Idade do atleta', example=25)]
+    peso: Annotated[PositiveFloat, Field(description='Peso do atleta', example=75.5)]
+    altura: Annotated[PositiveFloat, Field(description='Altura do atleta', example=1.70)]
+    sexo: Annotated[str, Field(description='Sexo do atleta', example='M', max_length=1)]
+    categoria: Annotated[CategoriaIn, Field(description='Categoria do atleta')]
+    centro_treinamento: Annotated[CentroTreinamentoAtleta, Field(description='Centro de treinamento do atleta')]
+
+
+class AtletaIn(Atleta):
+    pass
+
+
+class AtletaOut(Atleta, OutMixin):
+    pass
+
+class AtletaUpdate(BaseSchema):
+    nome: Annotated[Optional[str], Field(None, description='Nome do atleta', example='Joao', max_length=50)]
+    idade: Annotated[Optional[int], Field(None, description='Idade do atleta', example=25)]
